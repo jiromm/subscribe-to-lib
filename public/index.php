00794d2412126383c6e65717c53bbc478fbf5a18
Aram Baghdasaryan <xquack@gmail.com>
2014-10-19 21:28:46 +0400

    Google analytics added

--- public/index.php
+++ public/index.php
@@ -14,6 +14,15 @@ $libraries = include(dirname(__DIR__) . '/general/get-libraries.php');
 	<script src="js/modernizr.custom.js"></script>
 </head>
 <body>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-55896642-1', 'auto');
+	ga('send', 'pageview');
+</script>
 	<div id="intro">
 		<div class="intro-text">
 			<div class="container">
